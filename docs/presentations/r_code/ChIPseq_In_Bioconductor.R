d08fc73aa5585191c7599544ef43c196303fc3a0
root <tc.infomatics@gmail.com>
2020-07-19 17:11:32 +0000

    Autobuild

---
+++ docs/presentations/r_code/ChIPseq_In_Bioconductor.R
@@ -0,0 +1,362 @@
+params <-
+list(isSlides = "no")
+
+## ----include=FALSE------------------------------------------------------------
+suppressPackageStartupMessages(require(knitr))
+knitr::opts_chunk$set(echo = TRUE, tidy = T)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides != "yes"){
+  cat("# ChIPseq (part 1)
+
+
+"    
+  )
+  
+}
+
+
+
+## ----setup2, include=FALSE,eval=FALSE,echo=FALSE------------------------------
+## library(ShortRead)
+## 
+## fqSample <- FastqSampler("~/Downloads/ENCFF001NQP.fastq.gz",n=10^6)
+## temp <- yield(fqSample)
+## 
+## 
+## writeFastq(fastqSample,file = "~/Projects/Software/Github/RUBioconductor_Introduction/r_course/Data/sampled_ENCFF000CXH.fastq.gz",mode = "w")
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Working with raw ChIPseq data
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Working with raw ChIPseq data
+
+
+"    
+  )
+  
+}
+
+
+
+## ----shortreada,include=FALSE-------------------------------------------------
+library(ShortRead)
+
+
+
+## ----shortread, warning=F, message=F------------------------------------------
+library(ShortRead)
+
+
+## ---- echo=F,eval=F-----------------------------------------------------------
+## fqSample <- FastqSampler("~/Downloads/ENCFF001NQP.fastq.gz",n=10^6)
+## fastq <- yield(fqSample)
+## 
+## writeFastq(fastq,file = "~/Documents/Box Sync/RU/Teaching/RU_side/RU_ChIPseq/chipseq/inst/extdata/data/sampled_ENCFF001NQP.fastq.gz",mode = "w")
+## 
+
+
+## ----eval=T, echo=F-----------------------------------------------------------
+fastq <- readFastq(dirPath = "data/sampled_ENCFF001NQP.fastq.gz")
+
+
+## ----mycRep1Reads,echo=T,eval=F-----------------------------------------------
+## fqSample <- FastqSampler("~/Downloads/ENCFF001NQP.fastq.gz",n=10^6)
+## fastq <- yield(fqSample)
+
+
+## ----mycRep1ReadsShortReadQ,cache=TRUE,dependson="mycRep1Reads"---------------
+fastq
+
+
+## ----mycRep1ReadsAccessor,cache=TRUE,dependson="mycRep1Reads"-----------------
+readSequences <- sread(fastq)
+readQuality <- quality(fastq)
+readIDs <- id(fastq)
+readSequences
+
+
+## ----mycRep1ReadsQScores,cache=TRUE,dependson="mycRep1Reads"------------------
+readQuality <- quality(fastq)
+readQualities <- alphabetScore(readQuality)
+readQualities[1:10]
+
+
+## ----mycRep1ReadsQScoresPlot,cache=TRUE,dependson="mycRep1ReadsQScores",fig.height=3,fig.width=8----
+library(ggplot2)
+toPlot <- data.frame(ReadQ=readQualities)
+ggplot(toPlot,aes(x=ReadQ))+geom_histogram()+theme_minimal()
+
+
+## ----mycRep1ReadsAlpFreq,cache=TRUE,dependson="mycRep1Reads"------------------
+readSequences <- sread(fastq)
+readSequences_AlpFreq <- alphabetFrequency(readSequences)
+readSequences_AlpFreq[1:3,]
+
+
+## ----mycRep1ReadsAlpFreqSum,cache=TRUE,dependson="mycRep1ReadsAlpFreq"--------
+summed__AlpFreq  <- colSums(readSequences_AlpFreq)
+summed__AlpFreq[c("A","C","G","T","N")]
+
+
+## ----mycRep1ReadsAlpByCycle,cache=TRUE,dependson="mycRep1ReadsAlpFreq"--------
+readSequences_AlpbyCycle <- alphabetByCycle(readSequences)
+readSequences_AlpbyCycle[1:4,1:10]
+
+
+## ----mycRep1ReadsAlpByCyclePlot,cache=TRUE,dependson="mycRep1ReadsAlpFreq"----
+AFreq <- readSequences_AlpbyCycle["A",]
+CFreq <- readSequences_AlpbyCycle["C",]
+GFreq <- readSequences_AlpbyCycle["G",]
+TFreq <- readSequences_AlpbyCycle["T",]
+toPlot <- data.frame(Count=c(AFreq,CFreq,GFreq,TFreq),
+                     Cycle=rep(1:36,4),
+                     Base=rep(c("A","C","G","T"),each=36))
+
+
+
+## ----mycRep1ReadsAlpByCyclePlot2,cache=TRUE,eval=FALSE,dependson="mycRep1ReadsAlpByCyclePlot",fig.height=4,fig.width=8----
+## 
+## ggplot(toPlot,aes(y=Count,x=Cycle,colour=Base))+geom_line()+
+##   theme_bw()
+
+
+## ----mycRep1ReadsAlpByCyclePlot3,cache=TRUE,echo=FALSE,dependson="mycRep1ReadsAlpByCyclePlot",fig.height=4,fig.width=8----
+
+ggplot(toPlot,aes(y=Count,x=Cycle,colour=Base))+geom_line()+ylim(150000,400000)+
+  theme_bw()
+
+
+## ----mycRep1ReadsQByCycle,cache=TRUE,dependson="mycRep1ReadsAlpFreq"----------
+qualAsMatrix <- as(readQuality,"matrix")
+qualAsMatrix[1:2,]
+
+
+## ----mycRep1ReadsQByCyclePlot,cache=TRUE,dependson="mycRep1ReadsQByCycle",fig.width=8,fig.height=4----
+boxplot(qualAsMatrix[1:1000,])
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Filtering data
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Filtering data
+
+
+"    
+  )
+  
+}
+
+
+
+## ----out,eval=FALSE-----------------------------------------------------------
+## fqStreamer <- FastqStreamer("~/Downloads/ENCFF001NQP.fastq.gz",
+##                             n=100000)
+
+
+## ----out1,eval=FALSE----------------------------------------------------------
+## TotalReads <- 0
+## TotalReadsFilt <- 0
+## while (length(fq <- yield(fqStreamer))>0) {
+##     TotalReads <- TotalReads+length(fq)
+##     filt1 <- fq[alphabetScore(fq) > 300 ]
+##     filt2 <- filt1[alphabetFrequency(sread(filt1))[,"N"] < 10]
+##     TotalReadsFilt <- TotalReadsFilt+length(filt2)
+##     writeFastq(filt2,"filtered_ENCFF001NQP.fastq.gz",mode="a")
+## }
+
+## ----echo=F,eval=T------------------------------------------------------------
+TotalReads<-25555179
+TotalReadsFilt<-22864597
+
+
+## ----out11,eval=T,echo=T------------------------------------------------------
+TotalReads
+TotalReadsFilt
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Aligning data
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Aligning data
+
+
+"    
+  )
+  
+}
+
+
+
+## ----fa1q, include=FALSE------------------------------------------------------
+library(BSgenome.Mmusculus.UCSC.mm10)
+
+
+## ----fa1, echo=TRUE-----------------------------------------------------------
+library(BSgenome.Mmusculus.UCSC.mm10)
+BSgenome.Mmusculus.UCSC.mm10
+
+
+## ----fa2,cache=FALSE,echo=TRUE------------------------------------------------
+mainChromosomes <- paste0("chr",c(1:19,"X","Y","M"))
+mainChrSeq <- lapply(mainChromosomes,
+                     function(x)BSgenome.Mmusculus.UCSC.mm10[[x]])
+names(mainChrSeq) <- mainChromosomes
+mainChrSeqSet <- DNAStringSet(mainChrSeq)
+mainChrSeqSet
+
+
+## ----fa3, echo=TRUE,eval=FALSE------------------------------------------------
+## writeXStringSet(mainChrSeqSet,
+##                 "BSgenome.Mmusculus.UCSC.mm10.mainChrs.fa")
+
+
+## ---- echo=TRUE,eval=FALSE----------------------------------------------------
+## library(Rsubread)
+## buildindex("mm10_mainchrs","BSgenome.Mmusculus.UCSC.mm10.mainChrs.fa",
+##            memory=8000,
+##            indexSplit=TRUE)
+## 
+
+
+## ---- echo=F, eval=F----------------------------------------------------------
+## myMapped <- align("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/mm10_mainchrs",
+##                     "filtered_ENCFF001NQP.fastq.gz",
+##                     output_format = "BAM",
+##                     output_file = "Myc_Mel_1.bam",
+##                     nthreads = 4)
+## 
+
+
+## ---- echo=TRUE,eval=FALSE----------------------------------------------------
+## 
+## myMapped <- align("mm10_mainchrs",
+##                     "filtered_ENCFF001NQP.fastq.gz",
+##                     output_format = "BAM",
+##                     output_file = "Myc_Mel_1.bam",
+##                     type='dna',
+##                     phredOffset = 64,
+##                     nthreads = 4)
+## 
+
+
+## ----sampleTabless1, echo=TRUE,eval=FALSE-------------------------------------
+## library(Rbowtie2)
+
+
+## ----bsgecdnoaame, echo=TRUE,eval=FALSE---------------------------------------
+## bowtie2_build(references="BSgenome.Mmusculus.UCSC.mm10.mainChrs.fa",
+##                        bt2Index=file.path("BSgenome.Mmusculus.UCSC.mm10.mainChrs"))
+
+
+## ----bsgcdcenoaame, echo=TRUE,eval=FALSE--------------------------------------
+## library(R.utils)
+## gunzip("filtered_ENCFF001NQP.fastq.gz",
+##        remove=FALSE)
+## 
+## bowtie2(bt2Index = "BSgenome.Mmusculus.UCSC.mm10.mainChrs",
+##           samOutput = "ENCFF001NQP.sam",
+##           seq1 = "filtered_ENCFF001NQP.fastq")
+
+
+## ----bsgenoaaxssme, echo=TRUE,eval=FALSE--------------------------------------
+## bowtieBam <- asBam("ENCFF001NQP.sam")
+
+
+## ----bsgxxxnoaaxssme, echo=TRUE,eval=FALSE------------------------------------
+## unlink("ENCFF001NQP.sam")
+## 
+
+
+## ----sortindex, echo=TRUE,eval=FALSE------------------------------------------
+## library(Rsamtools)
+## sortBam("Myc_Mel_1.bam","SR_Myc_Mel_rep1")
+## indexBam("SR_Myc_Mel_rep1.bam")
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Mapped data
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Mapped data
+
+
+"    
+  )
+  
+}
+
+
+
+## ---- eval=F, echo=F----------------------------------------------------------
+## mappedReads <- idxstatsBam("SR_Myc_Mel_1.bam")
+## save(mappedReads, file="data/idxstatsBam_MycMel.RData")
+
+
+## ----mapped1, echo=TRUE,eval=FALSE--------------------------------------------
+## mappedReads <- idxstatsBam("SR_Myc_Mel_rep1.bam")
+## TotalMapped <- sum(mappedReads[,"mapped"])
+## ggplot(mappedReads,aes(x=seqnames,y=mapped))+
+##   geom_bar(stat="identity")+coord_flip()
+
+
+## ----mapped, echo=FALSE,eval=TRUE,fig.width=4,fig.height=4--------------------
+load("data/idxstatsBam_MycMel.RData")
+TotalMapped <- sum(mappedReads[,"mapped"])
+suppressPackageStartupMessages(library(ggplot2))
+ggplot(mappedReads,aes(x=seqnames,y=mapped))+geom_bar(stat="identity")+coord_flip()
+
+
+## ----coverage, echo=TRUE,eval=FALSE-------------------------------------------
+## forBigWig <- coverage("SR_Myc_Mel_rep1.bam")
+## forBigWig
+
+
+## ----bw, echo=TRUE,eval=FALSE-------------------------------------------------
+## library(rtracklayer)
+## export.bw(forBigWig,con="SR_Myc_Mel_rep1.bw")
+
+
+## ----weightedCover, echo=TRUE,eval=FALSE--------------------------------------
+## forBigWig <- coverage("SR_Myc_Mel_rep1.bam",
+##                       weight = (10^6)/TotalMapped)
+## forBigWig
+## export.bw(forBigWig,con="SR_Myc_Mel_rep1_weighted.bw")
+
---
+++ r_course/presentations/r_code/ChIPseq_In_Bioconductor2.R
@@ -0,0 +1,406 @@
+params <-
+list(isSlides = "no")
+
+## ----include=FALSE------------------------------------------------------------
+suppressPackageStartupMessages(require(knitr))
+knitr::opts_chunk$set(echo = TRUE, tidy = T)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides != "yes"){
+  cat("# ChIPseq (part 2)
+
+
+"    
+  )
+  
+}
+
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Quality Control
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Quality Control
+
+
+"    
+  )
+  
+}
+
+
+
+## ----mycQCdwdwshowL,include=FALSE---------------------------------------------
+library(ChIPQC)
+
+
+## ----eval=F-------------------------------------------------------------------
+## QCresult <- ChIPQCsample(reads="/pathTo/myChIPreads.bam",
+##                          genome="mm10",
+##                          blacklist = "/pathTo/mm10_Blacklist.bed")
+
+
+## ----mycQC,cache=TRUE,eval=FALSE----------------------------------------------
+## library(ChIPQC)
+## toBlkList<-"~/Downloads/ENCFF547MET.bed.gz"
+## chipqc_MycMel_rep1 <- ChIPQCsample("SR_Myc_Mel_rep1.bam",
+##                          annotation = "mm10",
+##                          blacklist = toBlkList,
+##                          chromosomes = paste0("chr",1:10))
+## class(chipqc_MycMel_rep1)
+## 
+
+
+## ----mycQCsecret,eval=FALSE,echo=F--------------------------------------------
+## library(ChIPQC)
+## toBlkList<-"~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/ENCFF547MET.bed.gz"
+## chipqc_MycMel_rep1 <- ChIPQCsample("SR_Myc_Mel_rep1.bam",
+##                          annotation = "mm10",
+##                          blacklist = toBlkList,
+##                          chromosomes = paste0("chr",1:10))
+## save(chipqc_MycMel_rep1,file='~/Documents/Box Sync/RU/Teaching/RU_side/RU_ChIPseq/chipseq/inst/extdata/data/rep1.RData')
+
+
+## ----mycQCshowLa,echo=FALSE,eval=TRUE-----------------------------------------
+toBlkList<-"data/ENCFF547MET.bed.gz"
+library(ChIPQC)
+load(file='data/rep1.RData')
+class(chipqc_MycMel_rep1)
+
+
+## ----mycQCshow,eval=TRUE------------------------------------------------------
+chipqc_MycMel_rep1
+
+
+## ---- echo=F, eval=F----------------------------------------------------------
+## FQ_FILES<-paste0("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/ChIPseq_course/",c("ENCFF001NQP.fastq.gz","ENCFF001NQP.fastq.gz","ENCFF001NGC.fastq.gz","ENCFF001NGO.fastq.gz","ENCFF001NCH.fastq.gz","ENCFF001NCF.fastq.gz","ENCFF001NIM.fastq.gz"))
+## 
+## FQ_NAMES<-c("Myc_Mel_1.bam","Myc_Mel_2.bam","Myc_Ch12_1.bam","Myc_Ch12_2.bam","input_Mel_1.bam","input_Mel_2.bam","input_Ch12_1.bam")
+## 
+## myMapped <- align("~/Documents/Box Sync/RU/Teaching/Compilation/Genomes_And_Datasets/mm10/mm10_mainchrs",
+##                     FQ_FILES,
+##                     output_format = "BAM",
+##                     output_file = FQ_NAMES,
+##                     nthreads = 4)
+## 
+## library(Rsamtools)
+## library(stringr)
+## 
+## SR_FQ_NAMES<-paste0("SR_",FQ_NAMES)
+## SR_FQ_NAMES_1<-paste0("SR_",str_split(FQ_NAMES,".bam", simplify = T)[,1])
+## 
+## bplapply(1:length(SR_FQ_NAMES), function(x){
+## sortBam(FQ_NAMES[x], SR_FQ_NAMES_1[x])
+## indexBam(SR_FQ_NAMES[x])
+## })
+## 
+
+
+## ----mycQCshowd2,cache=TRUE,eval=FALSE,include=FALSE, echo=F------------------
+## FQ_NAMES<-c("Myc_Mel_1.bam","Myc_Mel_2.bam","Myc_Ch12_1.bam","Myc_Ch12_2.bam","input_Mel_1.bam","input_Mel_2.bam","input_Ch12_1.bam")
+## SR_FQ_NAMES<-paste0("SR_",FQ_NAMES)
+## bamsToQC <- SR_FQ_NAMES
+## myQC <- bplapply(bamsToQC,ChIPQCsample,
+##         annotation = "mm10",
+##         blacklist = toBlkList,
+##         chromosomes = paste0("chr",1:10))
+## names(myQC)<-bamsToQC
+## save(myQC, file="data/myQCnoPeaks.RData")
+## # tried to update, but ChIPQC is upset. so leave it for now and owrk with old chipqc object
+
+
+## ----mycQCshow2,cache=TRUE,eval=FALSE-----------------------------------------
+## bamsToQC <- c("Sorted_Myc_Ch12_1.bam","Sorted_Myc_Ch12_2.bam",
+##              "Sorted_Myc_MEL_1.bam","Sorted_Myc_MEL_2.bam",
+##              "Sorted_Input_MEL.bam","Sorted_Input_Ch12.bam")
+## myQC <- bplapply(bamsToQC,ChIPQCsample,
+##         annotation = "mm10",
+##         blacklist = toBlkList,
+##         chromosomes = paste0("chr",1:10))
+## names(myQC) <- bamsToQC
+
+
+## ----qcmetricsA,include=FALSE-------------------------------------------------
+load(file="data/myQCnoPeaks.RData")
+
+
+## ----qcmetrics,cache=FALSE,eval=TRUE------------------------------------------
+QCmetrics(myQC)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Assessing fragment length
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Assessing fragment length
+
+
+"    
+  )
+  
+}
+
+
+
+## ----qcmetridedecs,cache=FALSE,eval=TRUE,fig.width=6,fig.height=4-------------
+plotCC(myQC,facetBy = "Sample")
+
+
+## ----qcmetridecs,cache=FALSE,eval=TRUE----------------------------------------
+myMeta <- data.frame(Sample= names(myQC),
+                     Tissue=c("Ch12","Ch12","MEL","MEL","MEL","Ch12"),
+                     Antibody=c(rep("Myc",4),rep("Input",2)))
+myMeta
+
+
+## ----qcmetricsede,cache=FALSE,eval=TRUE,fig.width=6,fig.height=3--------------
+plotCC(myQC,facetBy = "Tissue",addMetaData = myMeta,
+       colourBy="Antibody")
+
+
+## ----qcmetricsrf,cache=FALSE,eval=TRUE,fig.width=6,fig.height=3---------------
+plotCC(myQC,facetBy = "Tissue",addMetaData = myMeta,
+       colourBy="Antibody")+theme_bw()+
+  ggtitle("ChIPQC results")
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Blacklists and SSD
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Blacklists and SSD
+
+
+"    
+  )
+  
+}
+
+
+
+## ----fig.width=6,fig.height=2,warning=FALSE,message=FALSE---------------------
+plotSSD(myQC)+xlim(0,5)
+
+
+## ----fig.width=6,fig.height=3,warning=FALSE,message=FALSE---------------------
+plotSSD(myQC)+xlim(0.2,0.8)
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Library complexity and enrichment
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Library complexity and enrichment
+
+
+"    
+  )
+  
+}
+
+
+
+## ----fig.width=6,fig.height=3,warning=FALSE,message=FALSE---------------------
+myFlags <- flagtagcounts(myQC)
+myFlags["DuplicateByChIPQC",]/myFlags["Mapped",]
+
+
+## ----warning=FALSE,message=FALSE,fig.width=8,fig.height=4---------------------
+p <- plotRegi(myQC)
+
+
+## ----warning=FALSE,fig.width=12,fig.height=6----------------------------------
+p
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Peak Calling
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Peak Calling
+
+
+"    
+  )
+  
+}
+
+
+
+## macs2 callpeak -t Sorted_Myc_MEL_1.bam
+
+##                –name Mel_Rep1
+
+##                –-outdir PeakDirectory
+
+##                -c Sorted_Input_MEL.bam
+
+## 
+
+## ----fig.height=5, fig.width=15,eval=FALSE------------------------------------
+## myChIP <- "Sorted_Myc_MEL_1.bam"
+## myControl <- "Sorted_Input_MEL.bam"
+## 
+## macsCommand <- paste0("macs2 callpeak -t ", myChIP,
+##                       " -n ", "Mel_Rep1",
+##                       " –-outdir ","PeakDirectory",
+##                       " -c ", myControl)
+## system(macsCommand)
+
+
+## ----eval=T,echo=F,  warning=FALSE,collapse=T---------------------------------
+macsPeaks <- "data/Mel1_peaks.xls"
+
+
+## ----eval=T,echo=F,  warning=FALSE,collapse=T---------------------------------
+macsPeaks_DF <- read.delim(macsPeaks)
+macsPeaks_DF[1:8,]
+
+
+## ----eval=F,echo=T,  warning=FALSE,collapse=T---------------------------------
+## macsPeaks <- "data/Mel1_peaks.xls"
+
+
+## ----eval=T,echo=T, eval=T, echo=T, warning=FALSE-----------------------------
+macsPeaks_DF <- read.delim(macsPeaks, comment.char="#")
+macsPeaks_DF[1:2,]
+
+
+## ----eval=T,echo=T, eval=T, echo=T, warning=FALSE-----------------------------
+library(GenomicRanges)
+macsPeaks_GR <- GRanges(
+ seqnames=macsPeaks_DF[,"chr"],
+ IRanges(macsPeaks_DF[,"start"],
+         macsPeaks_DF[,"end"]
+ )
+)
+macsPeaks_GR
+
+
+## ----eval=T,echo=T, eval=T, echo=T, warning=FALSE-----------------------------
+seqnames(macsPeaks_GR)
+ranges(macsPeaks_GR)
+
+
+## ----eval=T,echo=T, eval=T, echo=T, warning=FALSE-----------------------------
+mcols(macsPeaks_GR) <- macsPeaks_DF[,c("abs_summit", "fold_enrichment")]
+macsPeaks_GR
+
+
+## ----eval=T,echo=T, eval=T, echo=T, warning=FALSE-----------------------------
+library(rtracklayer)
+blkList <- import.bed(toBlkList)
+macsPeaks_GR <- macsPeaks_GR[!macsPeaks_GR %over% blkList] 
+
+
+## ---- results='asis',include=TRUE,echo=FALSE----------------------------------
+if(params$isSlides == "yes"){
+  cat("class: inverse, center, middle
+
+# Peak Annotation
+
+<html><div style='float:left'></div><hr color='#EB811B' size=1px width=720px></html> 
+
+
+"    
+  )
+}else{
+  cat("# Peak Annotation
+
+
+"    
+  )
+  
+}
+
+
+
+## ----include=FALSE------------------------------------------------------------
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(org.Mm.eg.db)
+library(GenomeInfoDb)
+library(ChIPseeker)
+
+
+
+## ----eval=F,echo=T, eval=T, echo=T, warning=FALSE,tidy=T,message=FALSE--------
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(org.Mm.eg.db)
+library(GenomeInfoDb)
+library(ChIPseeker)
+
+
+
+## ----eval=T,echo=T, message=FALSE,messages=FALSE, eval=T, echo=T, warning=FALSE----
+peakAnno <- annotatePeak(macsPeaks_GR, tssRegion=c(-500, 500), 
+                         TxDb=TxDb.Mmusculus.UCSC.mm10.knownGene, 
+                         annoDb="org.Mm.eg.db")
+class(peakAnno)
+
+
+## ----eval=T,echo=T, message=F,messages=F, eval=T, echo=T, warning=FALSE,tidy=T----
+peakAnno
+
+
+## ----eval=T,echo=T, eval=T, echo=T, warning=FALSE,tidy=T----------------------
+peakAnno_GR <- as.GRanges(peakAnno)
+peakAnno_DF <- as.data.frame(peakAnno)
+
+
+## ----eval=T,echo=T, eval=T, echo=T, warning=FALSE,tidy=T----------------------
+peakAnno_GR[1,]
+
+
+## ---- eval=T, echo=T, fig.height=5, fig.width=15, warning=FALSE, tidy=T-------
+plotAnnoBar(peakAnno)
+
+
+## ----eval=T,echo=T, eval=F, echo=T, warning=FALSE,fig.height=5, fig.width=15,tidy=T----
+## plotDistToTSS(peakAnno)
+
+
+## ---- eval=T, echo=T, fig.height=5, fig.width=15, warning=FALSE, tidy=T-------
+upsetplot(peakAnno, vennpie=F)
+
